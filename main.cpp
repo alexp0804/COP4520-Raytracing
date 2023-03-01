e8982144b928626beb02c45925431adc7e4eadb8
Pairjax <pairjax@protonmail.com>
2023-02-28 20:35:31 -0500

    Normals

--- main.cpp
+++ main.cpp
@@ -1,28 +1,39 @@
 #include "vec3.h"
 #include "color.h"
 #include "ray.h"
+#include "hittable.h"
+#include "sphere.h"
 
 #include <iostream>
 
 using namespace std;
 
-bool hit_sphere(const Point3& center, double radius, const Ray& r)
+double hit_sphere(const Point3& center, double radius, const Ray& r)
 {
     Vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b*b - 4 *a*c;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b * half_b - a * c;
 
-    return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-half_b - sqrt(discriminant)) / a;
+    }
 }
 
 Color ray_color(Ray r) {
-    if (hit_sphere(Point3(0, 0, -1), 0.5, r))
-        return Color(1, 0, 0);
-    
+    auto t = hit_sphere(Point3(0, 0, -1), 0.5, r);
+
+    if (t > 0.0) {
+        Vec3 N = normalized(r.at(t) - Vec3(0, 0, -1));
+        Color C = Color(N.x() + 1, N.y() + 1, N.z() + 1);
+        return 0.5 * C;
+    }
+
     Vec3 unit_dir = normalized(r.direction());
-    auto t = 0.5 * (unit_dir.y() + 1.0);
+    t = 0.5 * (unit_dir.y() + 1.0);
 
     Color a = Color(1, 1, 1);
     Color b = Color(0.5, 0.7, 1.0);
