9e5e3bfab3331ba72da21e12f1557a0c3e0196ff
GameDevSlate <josemendozamatheus@gmail.com>
2023-03-15 19:39:51 -0400

    Dielectric material creation.
    Created an initial implementation for dielectric materials.

--- material.h
+++ material.h
@@ -14,34 +14,30 @@ public:
 };
 
 class Lambertian : public Material {
-    public:
-        Lambertian(Color a) : albedo(a) {}
+public:
+	explicit Lambertian(Color a) : albedo(a) {}
 
-        virtual bool scatter(
-            Ray r_in, HitRecord& rec, Color& attenuation, Ray& scattered
-        ) const override {
-            auto scatter_direction = rec.normal + random_unit_vector();
+	bool scatter(Ray r_in, HitRecord& rec, Color& attenuation, Ray& scattered) const override {
+        auto scatter_direction = rec.normal + random_unit_vector();
 
-            if (scatter_direction.near_zero()) {
-                scatter_direction = rec.normal;
-            }
-
-            scattered = Ray(rec.p, scatter_direction);
-            attenuation = albedo;
-            return true;
+        if (scatter_direction.near_zero()) {
+            scatter_direction = rec.normal;
         }
 
-    private:
-        Color albedo;
+        scattered = Ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
+
+private:
+    Color albedo;
 };
 
 class Metal : public Material {
 public:
     Metal(Color a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
-    virtual bool scatter(
-        Ray r_in, HitRecord& rec, Color& attenuation, Ray& scattered
-    ) const override {
+    bool scatter(Ray r_in, HitRecord& rec, Color& attenuation, Ray& scattered) const override {
           Vec3 reflected = reflect(normalized(r_in.direction()), rec.normal);
           Vec3 scatterDirection = random_in_unit_sphere();
           scattered = Ray(rec.p, reflected + fuzz * scatterDirection);
@@ -52,6 +48,27 @@ public:
 private:
     Color albedo;
     double fuzz;
+};
+
+class Dielectric : public Material
+{
+public:
+	explicit Dielectric(const double index_of_refraction): ir(index_of_refraction){}
+
+    bool scatter(Ray r_in, HitRecord& rec, Color& attenuation, Ray& scattered) const override
+    {
+        attenuation = Color(1.0, 1.0, 1.0);
+        double refraction_ratio = rec.front_face ? 1.0 / ir : ir;
+
+        Vec3 unit_direction = normalized(r_in.direction());
+        Vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = Ray(rec.p, refracted);
+
+        return true;
+    }
+
+    double ir; // Index of Refraction
 };
 
 #endif
