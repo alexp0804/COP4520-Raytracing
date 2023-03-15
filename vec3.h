6d9122b6d620f4ccf817c4e1e1c12bce1478bc52
GameDevSlate <josemendozamatheus@gmail.com>
2023-03-15 19:14:55 -0400

    Vec3 modernization and refraction
    Removed some redundant keywords for the Vec3 class, as well as added a refract utility function.

--- vec3.h
+++ vec3.h
@@ -5,7 +5,7 @@
 #include <iostream>
 #include "rt_utilities.h"
 
-#define ENTRIES 3
+constexpr auto ENTRIES = 3;
 
 class Vec3 {
     // Vec3 fields
@@ -13,119 +13,130 @@ class Vec3 {
         double e[ENTRIES];
 
     // Vec3 methods
-    public:
-        Vec3() {
-          for (int i = 0; i < ENTRIES; i++)
-            e[i] = (double) 0;
-        }
-        Vec3(double x, double y, double z) {
-          e[0] = x;
-          e[1] = y;
-          e[2] = z;
-        }
+    Vec3() {
+      for (double& i : e)
+          i = static_cast<double>(0);
+    }
 
-        double x() { return e[0]; }
-        double y() { return e[1]; }
-        double z() { return e[2]; }
+	Vec3(const double x, const double y, const double z) {
+      e[0] = x;
+      e[1] = y;
+      e[2] = z;
+    }
 
-        double length_squared() {
-          return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
-        }
-        double length() {
-          return std::sqrt(length_squared());
-        }
+    [[nodiscard]] double x() const { return e[0]; }
 
-        bool near_zero() const {
-            // Return true if the vector is close to zero in all dimensions.
-            const auto s = 1e-8;
-            return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
-        }
+    [[nodiscard]] double y() const { return e[1]; }
 
-        // Vec3 operators
+    [[nodiscard]] double z() const { return e[2]; }
 
-        // Negate
-        Vec3 operator- () const { return Vec3(-e[0], -e[1], -e[2]); }
+    [[nodiscard]] double length_squared() const
+    {
+      return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
 
-        // Subscript
-        double operator[] (int i) const { return e[i]; }
-        double& operator[] (int i) { return e[i]; }
+    [[nodiscard]] double length() const
+    {
+      return std::sqrt(length_squared());
+    }
 
-        // Assign-add
-        Vec3& operator+= (const Vec3 &v) {
-          e[0] += v.e[0];
-          e[1] += v.e[1];
-          e[2] += v.e[2];
+    [[nodiscard]] bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+    }
 
-          return *this;
-        }
-        // Assign-subtract
-        Vec3 &operator-= (const Vec3 &v) {
-          return *this += (-v);
-        }
-        // Assign-multiply (by scalar)
-        Vec3& operator*= (const double s) {
-          e[0] *= s;
-          e[1] *= s;
-          e[2] *= s;
+    // Vec3 operators
 
-          return *this;
-        }
-        // Assign-divide (by scalar)
-        Vec3& operator/= (const double s) {
-          e[0] /= s;
-          e[1] /= s;
-          e[2] /= s;
+    // Negate
+    Vec3 operator- () const { return {-e[0], -e[1], -e[2]}; }
 
-          return *this;
-        }
+    // Subscript
+    double operator[] (const int i) const { return e[i]; }
+    double& operator[] (const int i) { return e[i]; }
 
-        // Add
-        inline Vec3 operator+ (const Vec3 &v) {
-          return Vec3(e[0] + v.e[0], e[1] + v.e[1], e[2] + v.e[2]);
-        }
-        // Subtract
-        inline Vec3 operator- (const Vec3 &v) {
-          return Vec3(e[0] - v.e[0], e[1] - v.e[1], e[2] - v.e[2]);
-        }
+    // Assign-add
+    Vec3& operator+= (const Vec3 &v) {
+      e[0] += v.e[0];
+      e[1] += v.e[1];
+      e[2] += v.e[2];
 
-        // Multiply (by scalar)
-        inline Vec3 operator* (double s) {
-          return Vec3(e[0] * s, e[1] * s, e[2] * s);
-        }
-        // Divide (by scalar)
-        inline Vec3 operator/ (double s) {
-          return Vec3(e[0] / s, e[1] / s, e[2] / s);
-        }
+      return *this;
+    }
+    // Assign-subtract
+    Vec3 &operator-= (const Vec3 &v) {
+      return *this += (-v);
+    }
+    // Assign-multiply (by scalar)
+    Vec3& operator*= (const double s) {
+      e[0] *= s;
+      e[1] *= s;
+      e[2] *= s;
 
-        // Multiply (by vector, element-wise)
-        inline Vec3 operator* (const Vec3 &v) {
-          return Vec3(e[0] * v.e[0], e[1] * v.e[1], e[2] * v.e[2]);
-        }
+      return *this;
+    }
+    // Assign-divide (by scalar)
+    Vec3& operator/= (const double s) {
+      e[0] /= s;
+      e[1] /= s;
+      e[2] /= s;
 
-        // Print
-        inline std::ostream& operator<< (std::ostream &out) {
-          return out << e[0] << ' ' << e[1] << ' ' << e[2] << std::endl;
-        }
+      return *this;
+    }
 
-        // Randoms
-        inline static Vec3 random() {
-          return Vec3(random_double(), random_double(), random_double());
-        }
+    // Add
+	Vec3 operator+ (const Vec3 &v) const
+    {
+      return {e[0] + v.e[0], e[1] + v.e[1], e[2] + v.e[2]};
+    }
+    // Subtract
+	Vec3 operator- (const Vec3 &v) const
+    {
+      return {e[0] - v.e[0], e[1] - v.e[1], e[2] - v.e[2]};
+    }
 
-        inline static Vec3 random(double min, double max) {
-          return Vec3(random_double(min, max), random_double(min, max), random_double(min, max));
-        }
+    // Multiply (by scalar)
+	Vec3 operator* (const double s) const
+    {
+      return {e[0] * s, e[1] * s, e[2] * s};
+    }
+    // Divide (by scalar)
+	Vec3 operator/ (const double s) const
+    {
+      return {e[0] / s, e[1] / s, e[2] / s};
+    }
+
+    // Multiply (by vector, element-wise)
+	Vec3 operator* (const Vec3 &v) const
+    {
+      return {e[0] * v.e[0], e[1] * v.e[1], e[2] * v.e[2]};
+    }
+
+    // Print
+	std::ostream& operator<< (std::ostream &out) const
+    {
+      return out << e[0] << ' ' << e[1] << ' ' << e[2] << std::endl;
+    }
+
+    // Randoms
+	static Vec3 random() {
+      return {random_double(), random_double(), random_double()};
+    }
+
+	static Vec3 random(const double min, const double max) {
+      return {random_double(min, max), random_double(min, max), random_double(min, max)};
+    }
 };
 
-inline Vec3 operator* (double s, Vec3 &v) {
+inline Vec3 operator* (const double s, const Vec3 &v) {
     return v * s;
 }
-inline Vec3 operator/ (double s, Vec3 &v) {
+inline Vec3 operator/ (const double s, const Vec3 &v) {
     return v / s;
 }
 
 // Does NOT normalize the given vector, just returns the unit vector.
-inline Vec3 normalized(Vec3 a) {
+inline Vec3 normalized(const Vec3 a) {
     return a / a.length();
 }
 
@@ -134,11 +145,11 @@ inline double dot(const Vec3 &a, const Vec3 &b) {
 }
 
 inline Vec3 cross(const Vec3 &a, const Vec3 &b) {
-    return Vec3(
-        a.e[1] * b.e[2] - a.e[2] * b.e[1],
+    return {
+	    a.e[1] * b.e[2] - a.e[2] * b.e[1],
         a.e[2] * b.e[0] - a.e[0] * b.e[2],
         a.e[0] * b.e[1] - a.e[1] * b.e[0]
-    );
+    };
 }
 
 inline Vec3 random_in_unit_sphere() {
@@ -149,7 +160,7 @@ inline Vec3 random_in_unit_sphere() {
   }
 }
 
-inline Vec3 random_in_hemisphere(Vec3 normal) {
+inline Vec3 random_in_hemisphere(const Vec3 normal) {
     Vec3 in_unit_sphere = random_in_unit_sphere();
 
     if (dot(in_unit_sphere, normal) > 0.0) {
@@ -163,8 +174,16 @@ Vec3 random_unit_vector() {
   return normalized(random_in_unit_sphere());
 }
 
-Vec3 reflect(Vec3 v, Vec3 n) {
+inline Vec3 reflect(const Vec3 v, const Vec3 n) {
   return v - 2 * dot(v,n) * n;
+}
+
+inline Vec3 refract(const Vec3& uv, const Vec3& n, double etai_over_etat)
+{
+    auto cost_theta = fmin(dot(-uv, n), 1.0);
+    Vec3 r_out_perp = etai_over_etat * (uv + cost_theta * n);
+    Vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
 }
 
 using Point3 = Vec3;
